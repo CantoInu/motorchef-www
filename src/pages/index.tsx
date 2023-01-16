c35205291eb7e096fa4fc04abea7263ae9c9b542
Colin Platt <colingplatt@gmail.com>
2023-01-16 14:23:48 +0100

    adding home components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,18 +10,28 @@ import {PageHeader} from "components/PageHeader"
 import Image from "next/image"
 import {Container} from 'components/Container';
 import {Balances} from 'components/Balances';
+import { Spacer } from 'components/Spacer';
+import { Button } from 'components/Button';
 
 export default function Home() {
   return (
     <Page>
       <PageHeader
-        icon={<Image src={icon} height={240} width={240} />}
+        icon={<Image src={icon} height={240} width={240} alt="WOOF" />}
         title="MotorChef is Ready"
         subtitle="Stake Forteswap LP tokens to farm your share of WCANTO!"
       />
       <Container>
         <Balances/>
       </Container>
+      <Spacer size="lg" />
+      <div
+        style={{
+          margin: '0 auto',
+        }}
+      >
+        <Button text="🧑‍🌾 Get to farming" to="/farms" variant="secondary" />
+      </div>
     </Page>
   );
 };
